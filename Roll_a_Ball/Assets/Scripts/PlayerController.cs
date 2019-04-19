993dad32089fcc5dc3c64236d818146b92b9a25c
sonohoshi <yd111544@gmail.com>
2019-04-19 23:29:04 +0900

    Roll a Ball end
    I'm nice

---
+++ Roll_a_Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 10;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("telp"))
+        {
+            transform.position = (new Vector3(-9, 1, -9));
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count :" + count.ToString();
+        if (count >= 12) winText.text = "You Win!"; 
+    }
+}
